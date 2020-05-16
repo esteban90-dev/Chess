a3426a370d0b8a999a3710c0a04f87a81ef39f76
Stephen Allard <stephencallard@gmail.com>
2020-05-16 04:33:14 -0500

    Add #location

--- lib/board.rb
+++ lib/board.rb
@@ -11,6 +11,22 @@ class Board
     grid[input[0]][input[1]]
   end
 
+  def location(piece)
+    i = 0
+    j = 0
+    result = []
+    while i < grid.length
+      while j < grid[i].length
+        result << [i,j] if grid[i][j] == piece
+        j += 1
+      end
+      i += 1
+      j = 0
+    end
+    return nil if result.empty?
+    result
+  end
+
 
   private
 
