0dd446b1ed5ed95eab199ba1929553ef9b26a996
Stephen Allard <stephencallard@gmail.com>
2020-05-18 09:20:16 -0500

    Add first #valid_destintions test that includes the Board object

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -1,11 +1,12 @@
 require "./lib/knight.rb"
+require "./lib/board.rb"
 
 describe Knight do
 
-  TestPiece = Struct.new(:color)
-  let(:white_piece) { TestPiece.new("white") }
-  let(:black_piece) { TestPiece.new("black") }
-  let(:board) { double("Board")} 
+
+  let(:grid) { Array.new(8){ Array.new(8) } }
+  TestPiece1 = Struct.new(:color, :name, :valid_destinations)
+
 
   context "#initialize" do
     it "Raises an exception when initialized with an empty {}" do 
@@ -14,6 +15,11 @@ describe Knight do
 
     it "Doesn't raise an exception when initialized with a proper {}" do 
       expect{ Knight.new({:color => 'black'}) }.not_to raise_error(KeyError)
+    end
+
+    it "Name is set to 'knight' by default" do
+      black_knight = Knight.new({:color => 'black'})
+      expect(black_knight.name).to eql('knight')
     end
 
     it "If black knight: symbol is set to black knight unicode character by default" do 
@@ -29,21 +35,17 @@ describe Knight do
 
   context "#valid_destinations" do
     it "Returns the proper destination(s) if located at [0,0] on an empty board" do
-      knight1 = Knight.new({:color=>"black"})
-      #allow(board).to receive(:location).and_return([0,0])
-      #allow(board).to receive(:valid_location?).and_return(true, true, false, false, false, false, false, false)
-      #allow(board).to receive(:contents).and_return(nil, nil)
-      #allow(board).to receive(:active_color_in_check?).and_return(false)
-      expect(knight1.valid_destinations(board)).to eql([[2,1],[1,2]])
+      black_knight = Knight.new({:color=>"black"})
+      grid[0][0] = black_knight
+      board1 = Board.new({:grid=>grid})
+      expect(black_knight.valid_destinations(board1)).to eql([[2,1],[1,2]])
     end
 
+=begin
     it "Returns the proper destination(s) if located at initial board configuration" do 
-      knight1 = Knight.new({:color=>"black"})
-      #allow(board).to receive(:location).and_return([0,1])
-      #allow(board).to receive(:valid_location?).and_return(true, false, false, false, false, false, false, true)
-      #allow(board).to receive(:contents).and_return(nil,nil)
-      #allow(board).to receive(:active_color_in_check?).and_return(false)
-      expect(knight1.valid_destinations(board)).to eql([[2,2],[2,0]])
+      #black_knight = Knight.new({:color=>"black"})
+    
+      #expect(knight1.valid_destinations(board)).to eql([[2,2],[2,0]])
     end
 
     it "Returns the proper destination(s) if black is in check and only moving to [5,4] will remove check" do 
@@ -65,6 +67,7 @@ describe Knight do
       #allow(board).to receive(:removes_check?).and_return(false, false, false, false, false, false, false, false)
       expect(knight1.valid_destinations(board)).to eql([])
     end
+=end    
 
 
 
