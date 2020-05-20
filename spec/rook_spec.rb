9933eeaaf1515436b98a715af414283638fe8a8a
Stephen Allard <stephencallard@gmail.com>
2020-05-20 08:46:17 -0500

    Add additional tests for #valid_destinations

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -43,6 +43,44 @@ describe Rook do
               [7, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7]]
       expect(rook1.valid_destinations(board1)).to eql(correct_destinations)
     end
+
+    it "Returns the proper destination(s) if located at [3,3] on an empty board" do
+      white_rook = Rook.new({:color=>"white"})
+      empty_grid[3][3] = white_rook
+      board1 = Board.new({:grid=>empty_grid})
+      correct_destinations = [[4, 3], [5, 3], [6, 3], [7, 3], [3, 4], [3, 5], 
+              [3, 6], [3, 7], [2, 3], [1, 3], [0, 3], [3, 2], [3, 1], [3, 0]]
+      expect(white_rook.valid_destinations(board1)).to eql(correct_destinations)
+    end
+
+    it "Returns the proper destination(s) if located at [3,3] and the board is also populated with allies and enemies" do
+      white_rook = Rook.new({:color=>"white"})
+      white_pawn = TestPiece1.new("white","pawn",[],"1")
+      white_knight = TestPiece1.new("white","knight",[],"1")
+      black_pawn_1 = TestPiece1.new("black","pawn",[],"1")
+      black_pawn_2 = TestPiece1.new("black","pawn",[],"2")
+      empty_grid[1][3] = black_pawn_1
+      empty_grid[3][4] = black_pawn_2
+      empty_grid[5][3] = white_pawn
+      empty_grid[3][3] = white_rook
+      empty_grid[3][1] = white_knight
+      board1 = Board.new({:grid=>empty_grid})
+      correct_destinations = [[4, 3], [3, 4], [2, 3], [1, 3], [3, 2]]
+      expect(white_rook.valid_destinations(board1)).to eql(correct_destinations)
+    end
+
+    it "Returns the proper destination(s) if in check" do
+      black_rook = Rook.new({:color=>"black"})
+      white_pawn = TestPiece1.new("white","pawn",[],"1")
+      white_knight = TestPiece1.new("white","knight",[[3,0]],"1")
+      black_king = TestPiece1.new("black","king",[],"1")
+      empty_grid[1][1] = white_knight
+      empty_grid[3][1] = black_rook
+      empty_grid[3][0] = black_king
+      board1 = Board.new({:grid=>empty_grid})
+      board1.swap_color
+      expect(black_rook.valid_destinations(board1)).to eql([[1, 1]])
+    end
   end
 
 end
