e748f374c99c4dea944886b2d32d244d4106ba2d
Stephen Allard <stephencallard@gmail.com>
2020-05-21 04:12:57 -0500

    Add #valid_destinations, #diagonals, and #horz_vert

--- lib/queen.rb
+++ lib/queen.rb
@@ -9,11 +9,87 @@ class Queen
 
   public
 
+  def valid_destinations(board)
+    #get all moves
+    current_position = board.location(self)
+    destinations = []
+    destinations = horz_vert(board, current_position)
+    destinations += diagonals(board, current_position)
+
+    #If in check, select only the locations that result in the removal of check
+    if board.active_color_in_check?
+      destinations.select!{ |destination| board.disables_check?(current_position, destination) } 
+    end
+
+    #If not in check, reject locations that would result in check
+    if !board.active_color_in_check?
+      destinations.each{ |destination| board.enables_check?(current_position, destination) }
+    end
+
+    destinations
+  end
+
   private
 
   def default_symbol
     return "\u265b" if color == 'black'
     "\u2655"
+  end
+
+  def horz_vert(board, position)
+    #return all horizontal and vertical positions until 
+    #board limit or another piece is reached.  
+    current_position = position
+    result = []
+    i = 0
+    y_direction = [1,0,-1, 0]
+    x_direction = [0,1, 0,-1]
+    
+    while i < y_direction.length
+      loop do
+        current_position = [current_position[0] + y_direction[i], current_position[1] + x_direction[i]]
+        break unless board.valid_location?(current_position)
+        break unless board.contents(current_position).nil?
+        result << current_position
+      end
+
+      #if last position is on the board and contains an enemy piece, add it too
+      if board.valid_location?(current_position) && board.contents(current_position)
+        result << current_position if board.contents(current_position).color != board.active_color
+      end
+
+      current_position = position
+      i += 1
+    end
+    result
+  end
+
+  def diagonals(board, position)
+    #return all diagonal positions until 
+    #board limit or another piece is reached.  
+    current_position = position
+    result = []
+    i = 0
+    y_direction = [1,1,-1,-1]
+    x_direction = [1,-1,1,-1]
+    
+    while i < y_direction.length
+      loop do
+        current_position = [current_position[0] + y_direction[i], current_position[1] + x_direction[i]]
+        break unless board.valid_location?(current_position)
+        break unless board.contents(current_position).nil?
+        result << current_position
+      end
+
+      #if last position is on the board and contains an enemy piece, add it too
+      if board.valid_location?(current_position) && board.contents(current_position)
+        result << current_position if board.contents(current_position).color != board.active_color
+      end
+
+      current_position = position
+      i += 1
+    end
+    result
   end
 
 end
