6445fbbb10faa201d9d170185fbcb361660bb32b
Stephen Allard <stephencallard@gmail.com>
2020-05-21 04:13:43 -0500

    Added tests for #valid_destinations

--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -33,4 +33,47 @@ describe Queen do
     end
   end
 
+  context "#valid_destinations" do
+    it "Returns the proper destination(s) if located at [0,0] on an empty board" do
+      white_queen = Queen.new({:color=>"white"})
+      empty_grid[0][0] = white_queen
+      board1 = Board.new({:grid=>empty_grid})
+      expected = [[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],
+                  [0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],
+                  [1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7]]
+      expect(white_queen.valid_destinations(board1)).to eql(expected)
+    end
+
+    it "Returns the proper destination(s) if located at [4,4] on an empty board" do
+      black_queen = Queen.new({:color=>"black"})
+      empty_grid[4][4] = black_queen
+      board1 = Board.new({:grid=>empty_grid})
+      board1.swap_color
+      expected = [[5,4],[6,4],[7,4],[4,5],[4,6],[4,7],[3,4],
+                  [2,4],[1,4],[0,4],[4,3],[4,2],[4,1],[4,0],
+                  [5,5],[6,6],[7,7],[5,3],[6,2],[7,1],[3,5],
+                  [2,6],[1,7],[3,3],[2,2],[1,1],[0,0]]
+      expect(black_queen.valid_destinations(board1)).to eql(expected)
+    end
+
+    it "Returns the proper destination(s) if located at [4,5] and the board is also populated with allies and enemies" do
+      black_queen = Queen.new({:color=>"queen"})
+      black_pawn = TestPiece1.new("black","pawn",[],"1")
+      white_pawn = TestPiece1.new("white","pawn",[],"1")
+      white_knight_1 = TestPiece1.new("white","knight",[],"1")
+      white_knight_2 = TestPiece1.new("white","knight",[],"2")
+      empty_grid[4][5] = black_queen
+      empty_grid[2][5] = black_pawn
+      empty_grid[4][2] = white_knight_1
+      empty_grid[5][5] = white_pawn
+      empty_grid[5][6] = white_knight_2
+      board1 = Board.new({:grid=>empty_grid})
+      board1.swap_color
+      expected = [[5,5],[4,6],[4,7],[3,5],[4,4],[4,3],[4,2],
+                  [5,6],[5,4],[6,3],[7,2],[3,6],[2,7],[3,4],
+                  [2,3],[1,2],[0,1]]
+      expect(black_queen.valid_destinations(board1)).to eql(expected)
+    end
+  end
+
 end
