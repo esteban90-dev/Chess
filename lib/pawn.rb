9df9be79d3b8403e59514c06795c21bfb8f4cd00
Stephen Allard <stephencallard@gmail.com>
2020-05-22 05:44:45 -0500

    Add #valid_destinations

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -7,10 +7,35 @@ class Pawn
     @name = 'pawn'
   end
 
+  public
+
+  def valid_destinations(board)
+    current_position = board.location(self)
+
+    #get all possible destinations
+    destinations = all_destinations(board, current_position)
+
+    #If in check, select only the locations that result in the removal of check
+    if board.active_color_in_check?
+      destinations.select!{ |destination| board.disables_check?(current_position, destination) } 
+    end
+
+    #If not in check, reject locations that would result in check
+    if !board.active_color_in_check?
+      destinations.reject!{ |destination| board.enables_check?(current_position, destination) }
+    end
+
+    destinations
+  end
+
   private
 
   def default_symbol
     return "\u265f" if color == 'black'
     "\u2659"
+  end
+
+  def all_destinations(board, current_position)
+    
   end
 end
