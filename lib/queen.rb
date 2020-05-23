5860a6efa8cb23773064fc81ba63830a94b121f5
Stephen Allard <stephencallard@gmail.com>
2020-05-23 04:40:15 -0500

    Remove commented code

--- lib/queen.rb
+++ lib/queen.rb
@@ -1,38 +1,6 @@
 require "./lib/piece.rb"
 
-class Queen < Piece
-=begin  
-  attr_reader :color, :symbol, :name
-
-  def initialize(input)
-    @color = input.fetch(:color)
-    @symbol = input.fetch(:symbol, default_symbol)
-    @name = 'queen'
-  end
-
-  public
-
-  def valid_destinations(board)
-    current_position = board.location(self)
-
-    #get all possible destinations
-    destinations = all_destinations(board, current_position)
-
-    #If in check, select only the locations that result in the removal of check
-    if board.active_color_in_check?
-      destinations.select!{ |destination| board.disables_check?(current_position, destination) } 
-    end
-
-    #If not in check, reject locations that would result in check
-    if !board.active_color_in_check?
-      destinations.reject!{ |destination| board.enables_check?(current_position, destination) }
-    end
-
-    destinations
-  end
-
-=end  
-
+class Queen < Piece 
   private
 
   def default_symbol
