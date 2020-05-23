9b5fe89a1b3b50adc8194fa11cc4a62cd57355e6
Stephen Allard <stephencallard@gmail.com>
2020-05-23 06:27:53 -0500

    End of while loops - test for self.color instead of active_color

--- lib/queen.rb
+++ lib/queen.rb
@@ -25,7 +25,7 @@ class Queen < Piece
 
       #if last position is on the board and contains an enemy piece, add it too
       if board.valid_location?(new_position) && board.contents(new_position)
-        destinations << new_position if board.contents(new_position).color != board.active_color
+        destinations << new_position if board.contents(new_position).color != self.color
       end
       i += 1
     end
@@ -42,13 +42,14 @@ class Queen < Piece
         break unless board.contents(new_position).nil?
         destinations << new_position
       end
-
+      
       #if last position is on the board and contains an enemy piece, add it too
       if board.valid_location?(new_position) && board.contents(new_position)
-        destinations << new_position if board.contents(new_position).color != board.active_color
+        destinations << new_position if board.contents(new_position).color != self.color
       end
       i += 1
     end
+    
     destinations
   end
 
