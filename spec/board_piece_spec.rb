5363ded77dfffcd9b415cf2cd39b554f88dce562
Stephen Allard <stephencallard@gmail.com>
2020-05-24 07:59:51 -0500

    Move #move from board_spec to board_piece_spec

--- spec/board_piece_spec.rb
+++ spec/board_piece_spec.rb
@@ -117,4 +117,171 @@ describe "Board-Piece integration" do
       expect(board1.enables_check?([4,5],[4,6])).to eql(nil)
     end
   end
+
+  context "Board#move" do
+    it "moves a white piece that makes no capture" do
+      white_pawn = Pawn.new({:color=>'white'})
+      empty_grid[6][1] = white_pawn
+      board1 = Board.new({:grid=>empty_grid})
+      board1.move([6,1],[5,1])
+      expect(board1.history.last).to eql([white_pawn, [6,1], [5,1], nil, nil])
+    end
+
+    it "moves a white piece to capture a black piece" do
+      white_pawn = Pawn.new({:color=>'white'})
+      black_pawn = Pawn.new({:color=>'black'})
+      empty_grid[6][1] = white_pawn
+      empty_grid[5][2] = black_pawn
+      board1 = Board.new({:grid=>empty_grid})
+      board1.move([6,1],[5,2])
+      expect(board1.history.last).to eql([white_pawn, [6,1], [5,2], black_pawn, [5,2]])
+    end
+
+    it "moves a black piece that makes no capture" do
+      black_pawn = Pawn.new({:color=>'black'})
+      empty_grid[1][1] = black_pawn
+      board1 = Board.new({:grid=>empty_grid})
+      board1.swap_color
+      board1.move([1,1],[3,1])
+      expect(board1.history.last).to eql([black_pawn, [1,1], [3,1], nil, nil])
+    end
+
+    it "moves a black piece to capture a white piece" do
+      black_pawn = Pawn.new({:color=>'black'})
+      white_pawn = Pawn.new({:color=>'white'})
+      empty_grid[3][3] = black_pawn
+      empty_grid[4][4] = white_pawn
+      board1 = Board.new({:grid=>empty_grid})
+      board1.swap_color
+      board1.move([3,3],[4,4])
+      expect(board1.history.last).to eql([black_pawn, [3,3], [4,4], white_pawn, [4,4]])
+    end
+
+    it "moves a white pawn en_passant (+x direction) to capture a black piece" do
+      white_pawn = Pawn.new({:color=>'white'})
+      black_pawn = Pawn.new({:color=>'black'})
+      empty_grid[3][3] = white_pawn
+      empty_grid[3][4] = black_pawn
+      board1 = Board.new({:grid=>empty_grid})
+      board1.history << [black_pawn, [4,1], [4,3], nil, nil]
+      board1.move([3,3],[2,4])
+      expect(board1.history.last).to eql([white_pawn, [3,3], [2,4], black_pawn, [3,4]])
+    end
+
+    it "moves a white pawn en_passant (-x direction) to capture a black piece" do
+      white_pawn = Pawn.new({:color=>'white'})
+      black_pawn = Pawn.new({:color=>'black'})
+      empty_grid[3][4] = white_pawn
+      empty_grid[3][3] = black_pawn
+      board1 = Board.new({:grid=>empty_grid})
+      board1.history << [black_pawn, [3,1], [3,3], nil, nil]
+      board1.move([3,4],[2,3])
+      expect(board1.history.last).to eql([white_pawn, [3,4], [2,3], black_pawn, [3,3]])
+    end
+
+    it "moves a black pawn en_passant (+x direction) to capture a white piece" do
+      white_pawn = Pawn.new({:color=>'white'})
+      black_pawn = Pawn.new({:color=>'black'})
+      empty_grid[4][1] = black_pawn
+      empty_grid[4][2] = white_pawn
+      board1 = Board.new({:grid=>empty_grid})
+      board1.history << [white_pawn, [6,2], [6,4], nil, nil]
+      board1.swap_color
+      board1.move([4,1],[5,2])
+      expect(board1.history.last).to eql([black_pawn, [4,1], [5,2], white_pawn, [4,2]])
+    end
+
+    it "moves a black pawn en_passant (-x direction) to capture a white piece" do
+      white_pawn = Pawn.new({:color=>'white'})
+      black_pawn = Pawn.new({:color=>'black'})
+      empty_grid[4][3] = black_pawn
+      empty_grid[4][2] = white_pawn
+      board1 = Board.new({:grid=>empty_grid})
+      board1.history << [white_pawn, [6,2], [6,4], nil, nil]
+      board1.swap_color
+      board1.move([4,3],[5,2])
+      expect(board1.history.last).to eql([black_pawn, [4,3], [5,2], white_pawn, [4,2]])
+    end
+
+    it "moves a king and a rook in a king side castling move (white)" do
+      white_king = King.new({:color=>'white'})
+      white_rook_1 = Rook.new({:color=>'white'})
+      white_rook_2 = Rook.new({:color=>'white'})
+      empty_grid[7][0] = white_rook_2
+      empty_grid[7][4] = white_king
+      empty_grid[7][7] = white_rook_1
+      board1 = Board.new({:grid=>empty_grid})
+      board1.move([7,4],[7,6])
+      expect(board1.history[0..1]).to eql([[white_king, [7,4], [7,6], nil, nil], [white_rook_1, [7,7], [7,5], nil, nil]])
+    end
+
+    it "moves a king and a rook in a king side castling move (black)" do
+      black_king = King.new({:color=>'black'})
+      black_rook_1 = Rook.new({:color=>'black'})
+      black_rook_2 = Rook.new({:color=>'black'})
+      empty_grid[0][0] = black_rook_2
+      empty_grid[0][4] = black_king
+      empty_grid[0][7] = black_rook_1
+      board1 = Board.new({:grid=>empty_grid})
+      board1.swap_color
+      board1.move([0,4],[0,6])
+      expect(board1.history[0..1]).to eql([[black_king, [0,4], [0,6], nil, nil], [black_rook_1, [0,7], [0,5], nil, nil]])
+    end
+
+    it "moves a king and a rook in a queen side castling move (white)" do
+      white_king = King.new({:color=>'white'})
+      white_rook_1 = Rook.new({:color=>'white'})
+      white_rook_2 = Rook.new({:color=>'white'})
+      empty_grid[7][0] = white_rook_2
+      empty_grid[7][4] = white_king
+      empty_grid[7][7] = white_rook_1
+      board1 = Board.new({:grid=>empty_grid})
+      board1.move([7,4],[7,1])
+      expect(board1.history[0..1]).to eql([[white_king, [7,4], [7,1], nil, nil], [white_rook_2, [7,0], [7,2], nil, nil]])
+    end
+
+    it "moves a king and a rook in a queen side castling move (black)" do
+      black_king = King.new({:color=>'black'})
+      black_rook_1 = Rook.new({:color=>'black'})
+      black_rook_2 = Rook.new({:color=>'black'})
+      empty_grid[0][0] = black_rook_2
+      empty_grid[0][4] = black_king
+      empty_grid[0][7] = black_rook_1
+      board1 = Board.new({:grid=>empty_grid})
+      board1.swap_color
+      board1.move([0,4],[0,1])
+      expect(board1.history[0..1]).to eql([[black_king, [0,4], [0,1], nil, nil], [black_rook_2, [0,0], [0,2], nil, nil]])
+    end
+
+    it "records error message if the source cell is empty" do
+      board1 = Board.new({:grid=>empty_grid})
+      board1.move([0,0],[1,0])
+      expect(board1.history.last).to eql("invalid move - source cell empty")
+    end
+
+    it "records error message if the source cell is an enemy piece" do
+      black_rook = Rook.new({:color=>'black'})
+      empty_grid[0][4] = black_rook
+      board1 = Board.new({:grid=>empty_grid})
+      board1.move([0,4],[0,5])
+      expect(board1.history.last).to eql("invalid move - source cell contains enemy piece")
+    end
+
+    it "records error message if the source cell matches the destination cell" do
+      black_rook = Rook.new({:color=>'black'})
+      empty_grid[0][4] = black_rook
+      board1 = Board.new({:grid=>empty_grid})
+      board1.swap_color
+      board1.move([0,4],[0,4])
+      expect(board1.history.last).to eql("invalid move - source cell matches destination cell")
+    end
+
+    it "records error message if the source piece can't move to the destination" do
+      white_pawn = Pawn.new({:color=>'white'})
+      empty_grid[6][1] = white_pawn
+      board1 = Board.new({:grid=>empty_grid})
+      board1.move([6,1],[3,1])
+      expect(board1.history.last).to eql("invalid move - white pawn can't move from [6, 1] to [3, 1]")
+    end
+  end
 end
