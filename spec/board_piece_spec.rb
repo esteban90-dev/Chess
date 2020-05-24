8fdac04cd8a248e4f5a47624ad65c2e78c489090
Stephen Allard <stephencallard@gmail.com>
2020-05-23 22:24:22 -0500

    Moved Board#enables_check? and Board#disables_check? from board_spec to board_piece_spec

--- spec/board_piece_spec.rb
+++ spec/board_piece_spec.rb
@@ -74,4 +74,47 @@ describe "Board-Piece integration" do
       expect(board1.active_color_in_check?).to eql(false)
     end
   end
+
+  context "#disables_check?" do
+    it "returns true if the active color is in check and the given move disables a check condition" do
+      black_queen = Queen.new({:color=>'black'})
+      white_king = King.new({:color=>'white'})
+      empty_grid[0][3] = black_queen
+      empty_grid[3][3] = white_king
+      board1 = Board.new({:grid=>empty_grid})
+      expect(board1.disables_check?([3,3],[3,4])).to eql(true)
+    end
+
+    it "returns nil if the active color is not in check" do 
+      white_king = King.new({:color=>'white'})
+      white_pawn = Pawn.new({:color=>'white'})
+      black_pawn = Pawn.new({:color=>'black'})
+      empty_grid[3][7] = white_pawn
+      empty_grid[4][0] = white_king
+      empty_grid[3][6] = black_pawn
+      board1 = Board.new({:grid=>empty_grid})
+      expect(board1.disables_check?([3,7],[2,6])).to eql(nil)
+    end
+  end
+
+  context "#enables_check?" do
+    it "returns true if the active color is not check and the given move enables a check condition" do
+      black_king = King.new({:color=>'black'})
+      white_rook = Rook.new({:color=>'white'})
+      empty_grid[2][5] = white_rook
+      empty_grid[4][4] = black_king
+      board1 = Board.new({:grid=>empty_grid})
+      board1.swap_color
+      expect(board1.enables_check?([4,4],[4,5])).to eql(true)
+    end
+
+    it "returns nil if the active color is already in check" do 
+      white_king = King.new({:color=>'white'})
+      black_rook = Rook.new({:color=>'black'})
+      empty_grid[2][5] = black_rook
+      empty_grid[4][5] = white_king
+      board1 = Board.new({:grid=>empty_grid})
+      expect(board1.enables_check?([4,5],[4,6])).to eql(nil)
+    end
+  end
 end
