924a1a43e343da381813074098527099b4d531f5
Stephen Allard <stephencallard@gmail.com>
2020-05-24 08:06:44 -0500

    Remove tests for Board#under_threat?

--- spec/board_piece_spec.rb
+++ spec/board_piece_spec.rb
@@ -12,24 +12,6 @@ describe "Board-Piece integration" do
 
   let(:empty_grid) { Array.new(8){ Array.new(8) } }
 
-  context "Board#under_threat?" do
-    it "returns true if the given position can be reached by any enemy piece" do
-      black_rook = Rook.new({:color=>'black'})
-      empty_grid[0][5] = black_rook
-      board1 = Board.new({:grid=>empty_grid})
-      expect(board1.under_threat?([0,0])).to eql(true)
-    end
-
-    it "returns false if the given position can't be reached by any enemy piece" do
-      black_pawn = Pawn.new({:color=>'black'})
-      white_pawn = Pawn.new({:color=>'white'})
-      empty_grid[0][1] = black_pawn
-      empty_grid[2][2] = white_pawn
-      board1 = Board.new({:grid=>empty_grid})
-      expect(board1.under_threat?([3,3])).to eql(false)
-    end
-  end
-
   context "Board#active_color_in_check?" do
     it "returns true if the active color's (white) king position can be reached by any enemy pieces" do
       black_queen = Queen.new({:color=>'black'})
