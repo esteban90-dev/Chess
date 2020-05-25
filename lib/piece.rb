ec1e98ddad84b06d19bbe4867e22d184f679d6fa
Stephen Allard <stephencallard@gmail.com>
2020-05-25 04:16:02 -0500

    Add checks for destinations being empty

--- lib/piece.rb
+++ lib/piece.rb
@@ -20,12 +20,12 @@ attr_reader :color, :symbol, :name
     destinations = reachable_destinations(board)
   
     #If in check, select only the locations that result in the removal of check
-    if board.active_color_in_check?
+    if !destinations.empty? && board.active_color_in_check?
       destinations.select!{ |destination| board.disables_check?(current_position, destination) } 
     end
 
     #If not in check, reject locations that would result in check
-    if !board.active_color_in_check?
+    if !destinations.empty? && !board.active_color_in_check?
       destinations.reject!{ |destination| board.enables_check?(current_position, destination) }
     end
 
