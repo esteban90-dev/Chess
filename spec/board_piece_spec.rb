f8a2a5fcc9e3c9fc40ee1f546a498b7797237617
Stephen Allard <stephencallard@gmail.com>
2020-05-24 19:44:26 -0500

    Rename Knight#valid_destinations to #moveable_destinations and move tests to board_piece_spec

--- spec/board_piece_spec.rb
+++ spec/board_piece_spec.rb
@@ -407,4 +407,81 @@ describe "Board-Piece integration" do
       expect(white_rook.moveable_destinations(board1)).to eql([])
     end
   end
+
+  context "Knight#moveable_destinations" do 
+    it "Returns the proper destination(s) if located at [0,0] on an empty board" do
+      black_knight = Knight.new({:color=>"black"})
+      empty_grid[0][0] = black_knight
+      board1 = Board.new({:grid=>empty_grid})
+      board1.swap_color
+      expect(black_knight.moveable_destinations(board1)).to eql([[2,1],[1,2]])
+    end
+
+    it "Returns the proper destination(s) if located at [7,7] on an empty board" do
+      black_knight = Knight.new({:color=>"black"})
+      empty_grid[7][7] = black_knight
+      board1 = Board.new({:grid=>empty_grid})
+      board1.swap_color
+      expect(black_knight.moveable_destinations(board1)).to eql([[5,6],[6,5]])
+    end
+
+    it "Returns the proper destination(s) if surrounded by allies and enemies" do
+      white_knight = Knight.new({:color=>"white"})
+      white_pawn_1 = Pawn.new({:color=>"white"})
+      white_pawn_2 = Pawn.new({:color=>"white"})
+      white_pawn_3 = Pawn.new({:color=>"white"})
+      white_pawn_4 = Pawn.new({:color=>"white"})
+      black_pawn_1 = Pawn.new({:color=>"black"})
+      black_pawn_2 = Pawn.new({:color=>"black"})
+      black_pawn_3 = Pawn.new({:color=>"black"})
+      black_pawn_4 = Pawn.new({:color=>"black"})
+      empty_grid[3][4] = white_knight
+      empty_grid[1][3] = white_pawn_1
+      empty_grid[2][2] = black_pawn_1
+      empty_grid[4][2] = black_pawn_2
+      empty_grid[5][3] = white_pawn_2
+      empty_grid[5][5] = black_pawn_3
+      empty_grid[4][6] = white_pawn_3
+      empty_grid[2][6] = white_pawn_4
+      empty_grid[1][5] = black_pawn_4
+      board1 = Board.new({:grid=>empty_grid})
+      expect(white_knight.moveable_destinations(board1)).to eql([[5,5],[1,5],[2,2],[4,2]])
+    end
+
+    it "Returns the proper destination(s) if in check" do
+      white_knight = Knight.new({:color=>"white"})
+      white_king = King.new({:color=>"white"})
+      black_pawn = Pawn.new({:color=>"black"})
+      empty_grid[3][4] = white_king
+      empty_grid[2][3] = black_pawn
+      empty_grid[4][2] = white_knight
+      board1 = Board.new({:grid=>empty_grid})
+      expect(white_knight.moveable_destinations(board1)).to eql([[2,3]])
+    end
+
+    it "Removes destinations that would result in check" do
+      white_knight = Knight.new({:color=>"white"})
+      white_king = King.new({:color=>"white"})
+      black_bishop = Bishop.new({:color=>"black"})
+      empty_grid[3][3] = white_king
+      empty_grid[7][7] = black_bishop
+      empty_grid[5][5] = white_knight
+      board1 = Board.new({:grid=>empty_grid})
+      expect(white_knight.moveable_destinations(board1)).to eql([])
+    end
+
+    it "Returns [] if there are no valid destinations" do
+      black_knight = Knight.new({:color=>"black"})
+      black_pawn_1 = Pawn.new({:color=>"black"})
+      black_pawn_2 = Pawn.new({:color=>"black"})
+      black_pawn_3 = Pawn.new({:color=>"black"})
+      empty_grid[0][1] = black_knight
+      empty_grid[2][0] = black_pawn_1
+      empty_grid[2][2] = black_pawn_2
+      empty_grid[1][3] = black_pawn_3
+      board1 = Board.new({:grid=>empty_grid})
+      board1.swap_color
+      expect(black_knight.moveable_destinations(board1)).to eql([])
+    end
+  end
 end
