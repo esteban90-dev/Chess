891cf7fa46238463c95e920f52681e2e3a518545
Stephen Allard <stephencallard@gmail.com>
2020-05-29 16:26:53 -0500

    Add #game_over?

--- lib/game.rb
+++ lib/game.rb
@@ -8,6 +8,24 @@ class Game
 
   public
 
+  def play
+    loop do
+      console.write(board.formatted)
+      console.write(board.formatted_history)
+      input = console.prompt("Make a move:")
+      board.move_an(input)
+      board.swap_color
+      break if board.checkmate?
+      break if board.stalemate?
+    end
+  end
+
+  def game_over?
+    return true if board.checkmate?
+    return true if board.stalemate?
+    false
+  end
+
   private
 
 end
