60c8a9f003a86d09e0e2041e34161c0d71b1329d
Stephen Allard <stephencallard@gmail.com>
2020-05-29 16:27:03 -0500

    Add tests for #game_over?

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,7 +6,6 @@ describe Game do
   let (:console1) { double("console") }
 
   context "#initialize" do
-
     it "Raises an exception when initialized with an empty {}" do 
       expect{ Game.new({}) }.to raise_error(KeyError)
     end
@@ -14,7 +13,27 @@ describe Game do
     it "Does not raise an exception when initialized with a valid input {}" do
       expect{ Game.new({ :board =>  board1, :console => console1 }) }.not_to raise_error(KeyError)
     end
-
   end
 
+  context "#game_over?" do 
+    it "returns true if the board detects checkmate" do
+      allow(board1).to receive(:checkmate?).and_return(true)
+      game1 = Game.new({:board => board1, :console => console1 })
+      expect(game1.game_over?).to eql(true)
+    end
+
+    it "returns true if the board detects stalemate" do
+      allow(board1).to receive(:checkmate?).and_return(false)
+      allow(board1).to receive(:stalemate?).and_return(true)
+      game1 = Game.new({:board => board1, :console => console1 })
+      expect(game1.game_over?).to eql(true)
+    end
+
+    it "returns false if the board doesn't detect checkmate or stalemate" do
+      allow(board1).to receive(:checkmate?).and_return(false)
+      allow(board1).to receive(:stalemate?).and_return(false)
+      game1 = Game.new({:board => board1, :console => console1 })
+      expect(game1.game_over?).to eql(false)
+    end
+  end
 end
